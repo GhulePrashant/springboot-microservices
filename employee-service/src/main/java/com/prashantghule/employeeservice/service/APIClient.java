ae24040982674974186d91c97b5d22db6b7d8ec2
Prashant Ghule <84400771+GhulePrashant@users.noreply.github.com>
2024-02-04 11:28:03 +0530

    feature : Added Service Registry, API Gateway functionalities

--- employee-service/src/main/java/com/prashantghule/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/prashantghule/employeeservice/service/APIClient.java
@@ -5,6 +5,17 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
+/**
+ * Eureka server provides Load Balancer module to us.
+ * in @FeignClient annotation, use name attribute instead of url(hardcoded url value) and value.
+ * In the name attribute give the value which is configured in application.properties file of that microservice
+ * you can find it in localhost:8761 eureka UI also
+ *
+ * By using the microservice's registered name, if one of the instance is down,
+ * eureka will balance the load and give us response from available instance
+ */
+
 //@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
 @FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
